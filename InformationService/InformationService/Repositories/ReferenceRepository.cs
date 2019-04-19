ace3666cf16ce128f50e5312e096aaa750cd15a2
Van Van Lowe <van@nuttin-but.net>
2019-04-19 08:41:03 -0400

    add ReferenceRepository

---
+++ InformationService/InformationService/Repositories/ReferenceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InformationService.Interfaces;
+
+namespace InformationService.Repositories
+{
+    public class ReferenceRepository: IReferenceRepository
+    {
+    }
+}
