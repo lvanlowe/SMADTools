ec60b49c2522f262a4b12bf1fb8318f05a4fd72b
Van Van Lowe <van@nuttin-but.net>
2019-04-19 09:25:40 -0400

    add get all sports

--- InformationService/InformationService/Repositories/ReferenceRepository.cs
+++ InformationService/InformationService/Repositories/ReferenceRepository.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using InformationService.Interfaces;
+using InformationService.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace InformationService.Repositories
 {
-    public class ReferenceRepository: IReferenceRepository
+    public class ReferenceRepository : IReferenceRepository
     {
+        private PwsodbContext _context;
+
+        public ReferenceRepository(PwsodbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Sports>> GetAllSports()
+        {
+            var sports = await _context.Sports.ToListAsync();
+            return sports;
+        }
     }
 }
--- InformationService/InformationServiceTest/RepositoriesTest/ReferenceRepositoryTest.cs
+++ InformationService/InformationServiceTest/RepositoriesTest/ReferenceRepositoryTest.cs
@@ -1,10 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using InformationService.Models;
+using InformationService.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace InformationServiceTest.RepositoriesTest
 {
     public class ReferenceRepositoryTest
     {
+        private PwsodbContext _context;
+
+        public ReferenceRepositoryTest()
+        {
+            var options = new DbContextOptionsBuilder<PwsodbContext>().
+                UseInMemoryDatabase(databaseName: "ReferenceRepository")
+                .Options;
+            _context = new PwsodbContext(options);
+        }
+
+        private void InitializeSports()
+        {
+            var sports = _context.Sports.ToListAsync();
+            _context.RemoveRange(sports.Result);
+            _context.SaveChanges();
+        }
+
+        [Theory]
+        [InlineData(3)]
+        public void GetAllSports_When_executed_create_list_of_Sports(int last)
+
+        {
+            // Insert seed data into the database using one instance of the context
+            InitializeSports();
+
+            for (int i = 1; i < last + 1; i++)
+            {
+                _context.Sports.Add(new Sports() { Id = i, Name = "Baseball" + i, Email = i + "Jones@pwsova.org"  });
+            }
+
+            _context.SaveChanges();
+
+            var repository = new ReferenceRepository(_context);
+            var actual = repository.GetAllSports();
+
+            Assert.Equal(last, actual.Result.Count);
+
+        }
     }
 }
