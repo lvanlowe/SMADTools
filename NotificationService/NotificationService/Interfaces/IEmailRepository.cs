2996cbafd7899bbd8880eb0b0059870c97a4bd3a
Van Van Lowe <van@nuttin-but.net>
2019-06-19 08:44:27 -0400

    Add email interface

---
+++ NotificationService/NotificationService/Interfaces/IEmailRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+namespace NotificationService.Interfaces
+{
+    public interface IEmailRepository
+    {
+        Task<Response> SendEmail(EmailAddress from, EmailAddress to, EmailAddress cc, string subject, string plainTextContent,
+            string htmlContent);
+    }
+}
