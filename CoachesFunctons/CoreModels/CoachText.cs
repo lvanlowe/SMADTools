8cdd437fcb7c645135b66a0745f017b098217228
Van Van Lowe <van@nuttin-but.net>
2019-07-09 08:23:59 -0400

    add model for coaches text

---
+++ CoachesFunctons/CoreModels/CoachText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreModels
+{
+    public class CoachText
+    {
+        public string Message { get; set; }
+        public string Phone { get; set; }
+        public long ProgramId { get; set; }
+        public long? SportTypeId { get; set; }
+        public long? TeamId { get; set; }
+        public bool? Selected { get; set; }
+        public bool? IsVolunteer { get; set; }
+
+    }
+}
