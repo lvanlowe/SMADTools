57790ecf0ab1110043c890d272c49ef25457869d
Van Van Lowe <van@nuttin-but.net>
2019-07-16 08:42:45 -0400

    Add test project for worker

---
+++ CoachesFunctons/TrainingNotificationWorkerTest/EmailWorkerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TrainingNotificationWorkerTest
+{
+    public class EmailWorkerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
