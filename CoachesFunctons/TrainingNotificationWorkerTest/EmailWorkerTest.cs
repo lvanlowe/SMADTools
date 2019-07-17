1adc2b71cc00f7dbcea50ff0505b683c9a68dc59
Van Van Lowe <van@nuttin-but.net>
2019-07-17 09:49:32 -0400

    start adding test for getting emails by sport

--- CoachesFunctons/TrainingNotificationWorkerTest/EmailWorkerTest.cs
+++ CoachesFunctons/TrainingNotificationWorkerTest/EmailWorkerTest.cs
@@ -1,13 +1,29 @@
 using System;
+using Castle.Components.DictionaryAdapter;
+using InformationService.DataModels;
+using InformationService.Interfaces;
+using Moq;
+using TrainingNotificationWorker;
 using Xunit;
 
 namespace TrainingNotificationWorkerTest
 {
     public class EmailWorkerTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 7)]
+        public void GetEmailsForSport_When_executed_create_list_of_SportPhones(int sportId, int expected)
+
         {
+            // Insert seed data into the database using one instance of the context
+            //InitializeRegistrants();
+            //LoadRegistrants();
+            var mock = new Mock<ITrainingRepository>();
+            mock.Setup(repository => repository.GetEmailsBySport(sportId)).ReturnsAsync(new EditableList<SportEmails>());
+            var worker = new EmailWorker("connection string");
+            var actual = worker.GetEmailsForSport(sportId);
+
+            Assert.Equal(expected, actual.Result.Count);
 
         }
     }
--- TrainingNotificationWorker/EmailWorker.cs
+++ TrainingNotificationWorker/EmailWorker.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using InformationService.DataModels;
+using InformationService.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrainingNotificationWorker
 {
     public class EmailWorker
     {
-        public void SendEmailForSport()
-        {
 
+        private PwsodbContext _context;
+
+        public EmailWorker(string connectionString)
+        {
+            var options = new DbContextOptionsBuilder<PwsodbContext>().
+                UseSqlServer(connectionString, providerOptions => providerOptions.CommandTimeout(60))
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options;
+
+            _context = new PwsodbContext(options);
         }
 
 
+        public async Task<List<SportEmails>> GetEmailsForSport(int sportId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
