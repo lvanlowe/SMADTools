90b13af723d3182768255778d9de243f9d11bf1c
Van Van Lowe <van@nuttin-but.net>
2019-10-08 08:34:39 -0400

    added method to create dto

--- CoachesFunctons/TrainingManagingWorker/RegistrantWorker.cs
+++ CoachesFunctons/TrainingManagingWorker/RegistrantWorker.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using InformationService.Interfaces;
+using InformationService.Models;
+using InterfaceModels;
 
 namespace TrainingManagingWorker
 {
     public class RegistrantWorker
     {
+        private ITrainingRepository _trainingRepository;
+
+        public RegistrantWorker(ITrainingRepository trainingRepository)
+        {
+            _trainingRepository = trainingRepository;
+        }
+
+        public RegistrantDto PrepareRegistrantDataForClient(Registrant registrant)
+        {
+            RegistrantDto dto = new RegistrantDto();
+            dto.FirstName = registrant.FirstName;
+            return dto;
+        }
     }
 }
--- CoachesFunctons/TrainingManagingWorkerTest/RegistrantWorkerTest.cs
+++ CoachesFunctons/TrainingManagingWorkerTest/RegistrantWorkerTest.cs
@@ -1,8 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using InformationService.Interfaces;
+using InformationService.Models;
+using InterfaceModels;
+using Moq;
+using TrainingManagingWorker;
+using Xunit;
 
 namespace TrainingManagingWorkerTest
 {
     public class RegistrantWorkerTest
     {
+        private Mock<ITrainingRepository> _mockTrainingRepository;
+        private RegistrantWorker _worker;
+
+        public RegistrantWorkerTest()
+        {
+            _mockTrainingRepository = new Mock<ITrainingRepository>();
+            _worker = new RegistrantWorker(_mockTrainingRepository.Object);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PerpareRegistrantDataForClient_When_executed_return_dto(int sportId, int expected)
+
+        {
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2
+            };
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(registrant.FirstName, actual.FirstName);
+
+        }
     }
 }
