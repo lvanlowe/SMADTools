a567e9903b4cbf4acf6b6caff4e035fc1b3aa1b9
Van Van Lowe <van@nuttin-but.net>
2019-10-10 09:12:47 -0400

    add logicto prepare phone data

--- CoachesFunctons/TrainingManagingWorker/RegistrantWorker.cs
+++ CoachesFunctons/TrainingManagingWorker/RegistrantWorker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using InformationService.Interfaces;
 using InformationService.Models;
 using InterfaceModels;
@@ -18,7 +19,60 @@ namespace TrainingManagingWorker
         {
             RegistrantDto dto = new RegistrantDto();
             dto.FirstName = registrant.FirstName;
+            dto.LastName = registrant.LastName;
+            dto.NickName = registrant.NickName;
+            dto.Id = registrant.Id;
+            dto.SportId = registrant.SportId;
+            dto.ProgramId = registrant.ProgramId;
+            dto.SportTypeId = registrant.SportTypeId;
+            dto.Size = registrant.Size;
+            dto.IsVolunteer = registrant.IsVolunteer;
+            dto.Selected = registrant.Selected;
+            dto.TeamId = registrant.TeamId;
+            var athlete = registrant.RegisteredAthlete.FirstOrDefault();
+            if (athlete != null)
+            {
+                dto.RegisteredAthletesId = athlete.Id;
+                dto.AthletesId = athlete.AthletesId;
+            }
+
+            PreparePhones(registrant, dto);
+
             return dto;
+        }
+
+        private static void PreparePhones(Registrant registrant, RegistrantDto dto)
+        {
+            var phoneList = registrant.RegistrantPhone.ToList();
+            foreach (var phone in phoneList)
+            {
+                if (dto.RegistrantPhone1Id == 0)
+                {
+                    dto.Phone1 = phone.Phone;
+                    dto.CanText1 = phone.CanText;
+                    dto.PhoneType1 = phone.PhoneType;
+                    dto.RegistrantPhone1Id = phone.Id;
+                }
+                else
+                {
+                    if (dto.RegistrantPhone2Id == 0)
+                    {
+                        dto.Phone2 = phone.Phone;
+                        dto.CanText2 = phone.CanText;
+                        dto.PhoneType2 = phone.PhoneType;
+                        dto.RegistrantPhone2Id = phone.Id;
+                    }
+                    else
+                    {
+                        dto.Phone3 = phone.Phone;
+                        dto.CanText3 = phone.CanText;
+                        dto.PhoneType3 = phone.PhoneType;
+                        dto.RegistrantPhone3Id = phone.Id;
+                    }
+                }
+            }
         }
     }
 }
+
+
--- CoachesFunctons/TrainingManagingWorkerTest/RegistrantWorkerTest.cs
+++ CoachesFunctons/TrainingManagingWorkerTest/RegistrantWorkerTest.cs
@@ -38,7 +38,179 @@ namespace TrainingManagingWorkerTest
             };
             RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
             Assert.Equal(registrant.FirstName, actual.FirstName);
+            Assert.Equal(registrant.LastName, actual.LastName);
+            Assert.Equal(registrant.NickName, actual.NickName);
+            Assert.Equal(registrant.Id, actual.Id);
+            Assert.Equal(registrant.ProgramId, actual.ProgramId);
+            Assert.Equal(registrant.SportId, actual.SportId);
+            Assert.Equal(registrant.Size, actual.Size);
+            Assert.Equal(registrant.SportTypeId, actual.SportTypeId);
 
         }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PerpareRegistrantDataForClient_With_Optional_When_executed_return_dto(int sportId, int expected)
+
+        {
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2,
+                IsVolunteer = true,
+                Selected = true,
+                TeamId = 8
+            };
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(registrant.IsVolunteer, actual.IsVolunteer);
+            Assert.Equal(registrant.Selected, actual.Selected);
+            Assert.Equal(registrant.TeamId, actual.TeamId);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PerpareRegistrantDataForClient_With_Athlete_When_executed_return_dto(int sportId, int expected)
+
+        {
+            RegisteredAthlete athlete = new RegisteredAthlete{AthletesId = 219, Id = 67};
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2,
+                IsVolunteer = true,
+                Selected = true,
+                TeamId = 8,
+            };
+            registrant.RegisteredAthlete.Add(athlete);
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(athlete.Id, actual.RegisteredAthletesId);
+            Assert.Equal(athlete.AthletesId, actual.AthletesId);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PrepareRegistrantDataForClient_With_1_Phone_When_executed_return_dto(int sportId, int expected)
+
+        {
+            RegisteredAthlete athlete = new RegisteredAthlete { AthletesId = 219, Id = 67 };
+            RegistrantPhone phone1 = new RegistrantPhone { Phone = "703-555-1212", CanText = true, PhoneType = "cell", Id = 15};
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2,
+                IsVolunteer = true,
+                Selected = true,
+                TeamId = 8,
+            };
+            registrant.RegisteredAthlete.Add(athlete);
+            registrant.RegistrantPhone.Add(phone1);
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(phone1.Id, actual.RegistrantPhone1Id);
+            Assert.Equal(phone1.CanText, actual.CanText1);
+            Assert.Equal(phone1.Phone, actual.Phone1);
+            Assert.Equal(phone1.PhoneType, actual.PhoneType1);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PrepareRegistrantDataForClient_With_2_Phone_When_executed_return_dto(int sportId, int expected)
+
+        {
+            RegisteredAthlete athlete = new RegisteredAthlete { AthletesId = 219, Id = 67 };
+            RegistrantPhone phone1 = new RegistrantPhone { Phone = "703-555-1212", CanText = true, PhoneType = "cell", Id = 15 };
+            RegistrantPhone phone2 = new RegistrantPhone { Phone = "202-555-1212", CanText = false, PhoneType = "home", Id = 16 };
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2,
+                IsVolunteer = true,
+                Selected = true,
+                TeamId = 8,
+            };
+            registrant.RegisteredAthlete.Add(athlete);
+            registrant.RegistrantPhone.Add(phone1);
+            registrant.RegistrantPhone.Add(phone2);
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(phone1.Id, actual.RegistrantPhone1Id);
+            Assert.Equal(phone1.CanText, actual.CanText1);
+            Assert.Equal(phone1.Phone, actual.Phone1);
+            Assert.Equal(phone1.PhoneType, actual.PhoneType1);
+            Assert.Equal(phone2.Id, actual.RegistrantPhone2Id);
+            Assert.Equal(phone2.CanText, actual.CanText2);
+            Assert.Equal(phone2.Phone, actual.Phone2);
+            Assert.Equal(phone2.PhoneType, actual.PhoneType2);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void PrepareRegistrantDataForClient_With_3_Phone_When_executed_return_dto(int sportId, int expected)
+
+        {
+            RegisteredAthlete athlete = new RegisteredAthlete { AthletesId = 219, Id = 67 };
+            RegistrantPhone phone1 = new RegistrantPhone { Phone = "703-555-1212", CanText = true, PhoneType = "cell", Id = 15 };
+            RegistrantPhone phone2 = new RegistrantPhone { Phone = "202-555-1212", CanText = false, PhoneType = "home", Id = 16 };
+            RegistrantPhone phone3 = new RegistrantPhone { Phone = "301-555-1212", CanText = false, PhoneType = "other", Id = 34 };
+            Registrant registrant = new Registrant
+            {
+                FirstName = "Clark",
+                LastName = "Kent",
+                NickName = "Superman",
+                Id = 1,
+                ProgramId = 10,
+                SportId = 4,
+                Size = "small",
+                SportTypeId = 2,
+                IsVolunteer = true,
+                Selected = true,
+                TeamId = 8,
+            };
+            registrant.RegisteredAthlete.Add(athlete);
+            registrant.RegistrantPhone.Add(phone1);
+            registrant.RegistrantPhone.Add(phone2);
+            registrant.RegistrantPhone.Add(phone3);
+            RegistrantDto actual = _worker.PrepareRegistrantDataForClient(registrant);
+            Assert.Equal(phone1.Id, actual.RegistrantPhone1Id);
+            Assert.Equal(phone1.CanText, actual.CanText1);
+            Assert.Equal(phone1.Phone, actual.Phone1);
+            Assert.Equal(phone1.PhoneType, actual.PhoneType1);
+            Assert.Equal(phone2.Id, actual.RegistrantPhone2Id);
+            Assert.Equal(phone2.CanText, actual.CanText2);
+            Assert.Equal(phone2.Phone, actual.Phone2);
+            Assert.Equal(phone2.PhoneType, actual.PhoneType2);
+            //Assert.Equal(phone3.Id, actual.RegistrantPhone3Id);
+            //Assert.Equal(phone3.CanText, actual.CanText3);
+            //Assert.Equal(phone3.Phone, actual.Phone3);
+            //Assert.Equal(phone3.PhoneType, actual.PhoneType3);
+
+        }
+
     }
 }
