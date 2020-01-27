cb1ea50da1c48a18bdbd0fcde051fa4af5380377
lvanlowe <lvanlowe@inrvisions.com>
2020-01-27 10:13:09 -0500

    set up test get practices for locations

--- InformationService/InformationServiceTest/RepositoriesTest/CalendarRepositoryTest.cs
+++ InformationService/InformationServiceTest/RepositoriesTest/CalendarRepositoryTest.cs
@@ -21,28 +21,67 @@ namespace InformationServiceTest.RepositoriesTest
 
         }
 
+        private void InitializeCalendar()
+        {
+            var practice = _context.PracticeCalendarItems.ToListAsync();
+            _context.RemoveRange(practice.Result);
+            var calendar = _context.CalendarItems.ToListAsync();
+            _context.RemoveRange(calendar.Result);
+            var athletes = _context.RegisteredAthlete.ToListAsync();
+            _context.SaveChanges();
+        }
+
+        private void LoadCalendar()
+        {
+            var calendar1 = new CalendarItems() { Id = 1, ItemDate = DateTime.Now.AddDays(-7), };
+            var calendar2 = new CalendarItems() { Id = 2, ItemDate = DateTime.Now.AddDays(-3), };
+            var calendar3 = new CalendarItems() { Id = 3, ItemDate = DateTime.Now.AddDays(-1), };
+            var calendar4 = new CalendarItems() { Id = 4, ItemDate = DateTime.Now.AddDays(0), };
+            var calendar5 = new CalendarItems() { Id = 5, ItemDate = DateTime.Now.AddDays(4), };
+            var calendar6 = new CalendarItems() { Id = 6, ItemDate = DateTime.Now.AddDays(6), };
+            var calendar7 = new CalendarItems() { Id = 7, ItemDate = DateTime.Now.AddDays(7), };
+            var calendar8 = new CalendarItems() { Id = 8, ItemDate = DateTime.Now.AddDays(11), };
+            var calendar9 = new CalendarItems() { Id = 9, ItemDate = DateTime.Now.AddDays(13), };
+            var calendar10 = new CalendarItems() { Id = 10, ItemDate = DateTime.Now.AddDays(14), };
+
+            calendar1.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 1, CalendarItemId = 1, ProgramId = 1 });
+            calendar2.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 2, CalendarItemId = 2, ProgramId = 2 });
+            calendar4.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 3, CalendarItemId = 4, ProgramId = 1 });
+            calendar5.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 4, CalendarItemId = 5, ProgramId = 2 });
+            calendar7.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 5, CalendarItemId = 7, ProgramId = 1 });
+            calendar8.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 6, CalendarItemId = 8, ProgramId = 2 });
+            calendar10.PracticeCalendarItems.Add(new PracticeCalendarItems { Id = 7, CalendarItemId = 10, ProgramId = 1 });
+
+            _context.CalendarItems.Add(calendar1);
+            _context.CalendarItems.Add(calendar2);
+            _context.CalendarItems.Add(calendar3);
+            _context.CalendarItems.Add(calendar4);
+            _context.CalendarItems.Add(calendar5);
+            _context.CalendarItems.Add(calendar6);
+            _context.CalendarItems.Add(calendar7);
+            _context.CalendarItems.Add(calendar8);
+            _context.CalendarItems.Add(calendar9);
+            _context.CalendarItems.Add(calendar10);
+
+            _context.SaveChanges();
+
+        }
+
         [Theory]
-        [InlineData(3)]
-        //[InlineData(7)]
-        //[InlineData(10)]
-        public void GetPracticesForLocation_When_executed_create_list_of_Practice_Events(int locationId)
+        [InlineData(1, 3)]
+        //[InlineData(2, 2)]
+        public void GetPracticesForLocation_When_executed_create_list_of_Practice_Events(int locationId, int expected)
 
         {
             // Insert seed data into the database using one instance of the context
-            //InitializeSports();
+            InitializeCalendar();
+            LoadCalendar();
 
-            for (int i = 1; i < locationId + 1; i++)
-            {
-                _context.Sports.Add(new Sports() { Id = i, Name = "Baseball" + i, Email = i + "Jones@pwsova.org" });
-            }
-
-            _context.SaveChanges();
-            long programId = 1;
             DateTime startDate = DateTime.Now;
             var repository = new CalendarRepository(_context);
-            var actual = repository.GetPracticesForLocation(programId, startDate);
+            var actual = repository.GetPracticesForLocation(locationId, startDate);
 
-            Assert.Equal(locationId, actual.Result.Count);
+            Assert.Equal(expected, actual.Result.Count);
 
         }
 
