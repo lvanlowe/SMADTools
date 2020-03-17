9125d6badb7bd4e98ab906d4a64eaee9eac1564c
lvanlowe <lvanlowe@inrvisions.com>
2020-03-17 08:48:20 -0400

    add model for NotificationEntity

---
+++ InformationService/InformationService/Models/NotificationEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace InformationService.Models
+{
+    public class NotificationEntity : TableEntity
+    {
+        public NotificationEntity(string type, string name)
+        {
+            this.PartitionKey = type;
+            this.RowKey = name;
+        }
+        public NotificationEntity()
+        {
+            
+        }
+
+        public string Message { get; set; }
+    }
+}
