9ac916e418a4a72a10ba7b2da4ee7dcb940affdf
lvanlowe <lvanlowe@inrvisions.com>
2020-03-18 09:42:53 -0400

    Add Ref Repository

---
+++ InformationService/InformationServiceTest/RepositoriesTest/RefRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InformationService.Interfaces;
+using InformationService.Repositories;
+using Microsoft.Extensions.Configuration;
+
+namespace InformationServiceTest.RepositoriesTest
+{
+    public class RefRepositoryTest
+    {
+
+        IRefRepository _repository;
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+            return config;
+        }
+
+        public RefRepositoryTest()
+        {
+            var config = InitConfiguration();
+            var connectionString = config["ConnectionString"];
+            _repository = new RefRepository();
+
+        }
+    }
+}
