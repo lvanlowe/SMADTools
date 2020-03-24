542e68179eb222718a3c25e94056462bcf13af88
lvanlowe <lvanlowe@inrvisions.com>
2020-03-24 08:56:27 -0400

    added worker to recieve text function

--- CoachesFunctons/CoachesFunctons/ReceiveTextMessageFunc.cs
+++ CoachesFunctons/CoachesFunctons/ReceiveTextMessageFunc.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using InformationService.Interfaces;
+using InformationService.Models;
+using InformationService.Repositories;
+using InterfaceModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using TrainingManagingWorker;
+using TrainingNotificationWorker;
 using Twilio.TwiML;
 
 namespace CoachesFunctons
@@ -35,11 +42,33 @@ namespace CoachesFunctons
             log.LogInformation(formValues["From"]);
             //dynamic data = JsonConvert.DeserializeObject(requestBody);
             //from = from ?? data?.from;
+            EventTextDto dto = new EventTextDto();
+            dto.Message = formValues["Body"];
+            dto.From = formValues["From"];
+            dto.Zip = formValues["FromZip"];
+            dto.City = formValues["FromCity"];
+            string responseMessage = $"Hello, {formValues["Body"]}. This message is from the PWSO Notification System.";
 
+            try
+            {
+                var connectionString = System.Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TrainingModel");
+                var azureConnectionString = System.Environment.GetEnvironmentVariable("AzureStorageConnectionString");
+                var options = new DbContextOptionsBuilder<PwsodbContext>().UseSqlServer(connectionString).Options;
+                PwsodbContext context = new PwsodbContext(options);
+                ITrainingRepository trainingRepository = new TrainingRepository(context);
+                IRefRepository refRepository = new RefRepository(azureConnectionString);
+                RegistrantWorker worker = new RegistrantWorker(trainingRepository, refRepository);
+                var notification  = worker.AddNumberForEvent(dto);
+                responseMessage = notification.Result.Message;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
             //string smsinfo = data.ToString();
             //log.LogInformation(smsinfo);
 
-            string responseMessage = $"Hello, {formValues["Body"]}. This message is from the PWSO Notification System.";
 
             //var data = await req.Content.ReadAsStringAsync();
             //var formValues = data.Split('&')
@@ -58,7 +87,7 @@ namespace CoachesFunctons
             //{
             //    Content = new StringContent(twiml, Encoding.UTF8, "application/xml")
             //};
-
+            responseMessage += ". This message is from the PWSO Notification System.";
             return new OkObjectResult(responseMessage);
         }
     }
